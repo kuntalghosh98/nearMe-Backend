2a38b4f59de8d524bf0d25341d4fe13e0a998cf3
kuntalghosh98 <129258416+kuntalghosh98@users.noreply.github.com>
2023-03-31 15:21:19 +0530

    Update AppRESTController.java

--- src/main/java/com/nearMe/App/controller/AppRESTController.java
+++ src/main/java/com/nearMe/App/controller/AppRESTController.java
@@ -1,13 +1,23 @@
 package com.nearMe.App.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppRESTController {
 
+	@CrossOrigin()
 	@GetMapping("/List")
-	public String fetchffVendors() {
-		return "Vendor";
+	public List<String> fetchffVendors() {
+		List<String> ls1=new ArrayList<String>();
+		ls1.add("v1");
+		ls1.add("v2");
+		ls1.add("v3");
+		return ls1;
 	}
 }
