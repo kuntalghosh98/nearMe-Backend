b5b419de191fe1d7db7ed71501e822910267c024
kuntalghosh98 <129258416+kuntalghosh98@users.noreply.github.com>
2023-04-06 18:09:57 +0530

    Update AppApplication.java

--- src/main/java/com/nearMe/App/AppApplication.java
+++ src/main/java/com/nearMe/App/AppApplication.java
@@ -3,8 +3,13 @@ package com.nearMe.App;
 import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//@ComponentScan("com.nearMe.App.repo")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.nearMe.App.repo"})
 public class AppApplication {
 
 	public static void main(String[] args) {
