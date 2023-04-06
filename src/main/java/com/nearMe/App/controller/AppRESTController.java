c0a26ef8536662e6df70660f277e3a3f3dcfb041
kuntalghosh98 <129258416+kuntalghosh98@users.noreply.github.com>
2023-04-06 18:10:01 +0530

    Update AppRESTController.java

--- src/main/java/com/nearMe/App/controller/AppRESTController.java
+++ src/main/java/com/nearMe/App/controller/AppRESTController.java
@@ -1,15 +1,26 @@
 package com.nearMe.App.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nearMe.App.entities.LocationInfo;
+import com.nearMe.App.service.AppService;
 
 @RestController
 public class AppRESTController {
+	
+	@Autowired
+	private AppService apps;
+	
+	private static final Logger logger=LoggerFactory.getLogger(AppRESTController.class);
 
 	@CrossOrigin()
 	@GetMapping("/List")
@@ -20,4 +31,17 @@ public class AppRESTController {
 		ls1.add("v3");
 		return ls1;
 	}
+	
+	@GetMapping("/message")
+	public String message() {
+		return apps.getMessage();
+	}
+	
+	@GetMapping("/List/{id}")
+	public LocationInfo loca_inf(@PathVariable String id) {
+		logger.error("Controller Class : Request received for Location : "+id);
+		return apps.getLocationInfo(id);
+		
+	}
+	
 }
